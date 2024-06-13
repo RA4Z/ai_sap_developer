67d08912bf883aecfdbf95fe86679e88b696c840
RA4Z <rraz639@gmail.com>
2024-06-13 16:30:06 -0300

    Enviando código criado para pasta IAron

--- functions.py
+++ functions.py
@@ -1,3 +1,6 @@
+import shutil
+
+
 def retornar_codigo(texto: str):
     indice_inicio = texto.find("```python")
     if indice_inicio == -1:
@@ -13,6 +16,7 @@ def retornar_codigo(texto: str):
 
     return texto[indice_inicio:indice_fim]
 
+
 def pegar_texto_externo(texto: str):
     indice_inicio = texto.find("```python")
     if indice_inicio == -1:
@@ -27,3 +31,12 @@ def pegar_texto_externo(texto: str):
         return None
 
     return texto[texto.find("\n", indice_fim) + 1:]
+
+def copiar_pasta(origin: str, destiny: str):
+    try:
+        shutil.copytree(origin, destiny)
+        print(f"Pasta copiada com sucesso de '{origin}' para '{destiny}'")
+    except FileExistsError:
+        print(f"A pasta '{destiny}' já existe. A cópia foi cancelada.")
+    except Exception as e:
+        print(f"Erro ao copiar pasta: {e}")
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from gemini_config import develop_code
-from functions import retornar_codigo, pegar_texto_externo
+from functions import retornar_codigo, pegar_texto_externo, copiar_pasta
 
 if __name__ == '__main__':
     output = develop_code("COHV\nvariante /PS\nexecutar\narmazenar todos os dados da coluna \"PROJN\"\n\nCN47N\ncolar todos os dados salvos no campo \"Elemento PEP\"\nescrever \"/INI_MONT\" no campo \"Layout\"\nexecutar\nescrever dados da tabela na planilha chamada \"Esquema.xlsm\"")
@@ -8,3 +8,8 @@ if __name__ == '__main__':
 
     open('script/main.py', 'w', encoding="UTF-8").write(f"{codigo}")
     print(documentation)
+
+    origem = "C:\\Users\\robertn\\Documents\\Projetos\\PYTHON\\ai_sap_developer\\script"
+    destino = "C:\\IAron\\script"
+
+    copiar_pasta(origem, destino)
