6e23d9dbf5c20febd97fe7bb8bac4f573a0d2d98
RA4Z <rraz639@gmail.com>
2024-06-14 13:55:29 -0300

    Abrindo arquivo no Explorador de Arquivos diretamente após criação

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from gemini_config import develop_code
 from functions import retornar_codigo, pegar_texto_externo, copiar_pasta, cadastrar_doc
 import time
+import subprocess
 
 def run_automation(user_input: str):
     output = develop_code(user_input)
@@ -14,6 +15,7 @@ def run_automation(user_input: str):
 
     open(f'C:\\IAron\\{file}\\main.py', 'w', encoding="UTF-8").write(f"{codigo}")
     cadastrar_doc(documentation, destino)
+    subprocess.run(['explorer', destino])
 
 
 if __name__ == '__main__':
